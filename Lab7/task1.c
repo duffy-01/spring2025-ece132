715c20995bfa145e444851902821ac2709169b89
Shane Duffy <140999121+duffy-01@users.noreply.github.com>
2025-03-30 20:54:17 -0400

    Update task1.c

--- Lab7/task1.c
+++ Lab7/task1.c
@@ -3,13 +3,13 @@
 //Lab section: ECE 132, Section 010
 //*************************************************
 //Date Started: Tuesday, March 25, 2025
-//Date of Last Modification: March 25, 2025
+//Date of Last Modification: March 27, 2025
 //Lab Assignment: Lab 7
 //Lab Due Date: Tuesday, April 1, 2025
 //*************************************************
 //Purpose of program: Task 1, create a simple multimeter.
-//Program Inputs:
-//Program Outputs: 
+//Program Inputs: Analog voltage from ADC channel 0 (PE3)
+//Program Outputs: Voltage displayed over UART terminal
 //*************************************************
 
 //File include Statements
@@ -23,104 +23,134 @@
 #include "driverlib/gpio.h"
 #include "driverlib/timer.h"
 #include "driverlib/pin_map.h"
-#include "driverlib/sysctl.h"
 #include "driverlib/uart.h"
 #include "driverlib/adc.h"
 
-
-//Variable Declarations
+// Variable Declarations
 #define GPIO_PA0_U0RX           0x00000001
-
 #define GPIO_PA1_U0TX           0x00000401
 
-//Function Prototypes
+// Function Prototypes
+bool UARTKeyPressed(void); // Helper function to detect a key press
 
-//Main Program
+// Main Program
+// NOTE: any blank fills are notated in UPPERCASE for legibility
+
 int main(void) {
-    unsigned long INPUT;
-    char message[]="Voltage: ";
+    uint32_t INPUT;
+    float voltage;
+    unsigned long voltage_mV;
     int i;
+
+    char message[] = "Voltage: ";
     int mess_len = sizeof(message)/sizeof(message[0]);
-    char units[]= " V"; //Character array for the units of the voltage
+
+    char units[] = " V"; 				// char array for voltage units
     int un_len = sizeof(units)/sizeof(units[0]);
-    char next[]="Press any Key to sample again";
+
+    char next[] = "Press any key to sample again\r\n";
     int next_len = sizeof(next)/sizeof(next[0]);
-    long C;
 
-    //The following block of code is used to configure ____________________
-    SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ); // Set up Clock
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0); // Enable UART hardware
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA); // Enable Pin hardware
-    GPIOPinConfigure(GPIO_PA0_U0RX); // Configure GPIO pin for UART RX line
-    GPIOPinConfigure(GPIO_PA1_U0TX); // Configure GPIO Pin for UART TX line
-    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1); // Set Pins for UART
-    UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200, // Configure UART to 8N1 at 115200bps
-    (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+    // The following block of code is used to configure THE SYSTEM CLOCK AND UART0
+    SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ); // Set system clock to 50MHz
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0); // Enable UART0 module
+    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_UART0)) {} // Wait for UART0 to be ready
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA); // Enable GPIO Port A
+    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOA)) {} // Wait for GPIOA to be ready
+    GPIOPinConfigure(GPIO_PA0_U0RX); // Configure PA0 as UART RX line
+    GPIOPinConfigure(GPIO_PA1_U0TX); // Configure PA1 as UART TX line
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1); // Set PA0 and PA1 as UART pins
+    UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,
+        (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE)); // Set UART to 8N1 at 115200bps
 
+    // Send startup message to UART terminal for visual confirmation
+    char init_msg[] = "Ready! Press any key to start sampling.\r\n";
+    for (i = 0; i < sizeof(init_msg) - 1; i++) {
+        UARTCharPut(UART0_BASE, init_msg[i]);
+    }
 
+    // Peripheral setup for ADC0
+    // This will read the voltage off Port E Pin 3 (AIN0)
+    // This ADC will have the ability to read a voltage between 0V and 3V3
+    // The resolution of the measurement is ~0.8059 mV (3.3V / 4096 steps)
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0); // Enable ADC0 module
+    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_ADC0)) {} // Wait for ADC0 to be ready
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE); // Enable GPIO Port E
+    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOE)) {} // Wait for GPIOE to be ready
+    GPIOPinTypeADC(GPIO_PORTE_BASE, GPIO_PIN_3); // Enable PE3 as ADC input (AIN0)
+    ADCSequenceDisable(ADC0_BASE, 0); // Disable ADC0 before configuration
+    ADCSequenceConfigure(ADC0_BASE, 0, ADC_TRIGGER_PROCESSOR, 0); // The ADC0 is configured to use PROCESSOR TRIGGER and PRIORITY 0
+    
+    // The ADC will support up to 8 samples to be taken (WE ARE USING 1)
+    // The ADC will be triggered to get a sample based on the condition: PROCESSOR TRIGGER
+    // The ADC will have a priority that is HIGHER THAN OTHER SAMPLINGS (PRIORITY 0)
 
-    //Peripheral setup for ADC0
-    //This will read the voltage off Port ___ Pin ___
-	  //This ADC will have the ability to read a voltage between 0mV and ___ mV
-	  //The resolution of the measurement is __ mV
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_ADC0);
+    ADCSequenceStepConfigure(ADC0_BASE, 0, 0, ADC_CTL_CH0 | ADC_CTL_IE | ADC_CTL_END);
+    // For SEQUENCER 0, the step 0 configuration is being setup
+    // The step configuration is for CHANNEL 0 (AIN0) to be read and this is the sample that is LAST IN THE SEQUENCE READ
+    // It is configured that the ADC INTERRUPT IS ALLOWED TO HAPPEN
 
-    ADCSequenceConfigure(ADC0_BASE, 0, ADC_TRIGGER_PROCESSOR,0); //The ADC0 is configured
-    //The ADC will support up to ___ samples to be taken
-    //The ADC will be triggered to get a sample based on the condition _____________________
-    //The ADC will have a priority that is __________ than other samplings
+    ADCSequenceEnable(ADC0_BASE, 0); //Enable ADC0 with sample sequence number 0
 
-    ADCSequenceStepConfigure(_________,0,0,ADC_CTL_IE | ADC_CTL_END | ADC_CTL_CH0);
-    //For ____ the step configuration is being setup
-    //The step configuration is for Channel ___ to be read and this to be the sample that is ____ in the sequence read
-    //It is configured that the ADC interrupt is (allowed or not allowed) to happen
+    while (1) {
+        // waits for user key press to trigger ADC read
+        while (!UARTKeyPressed()) {}
 
-    ADCSequenceEnable(ADC0_BASE, 0); //Enable ADC___ with sample sequence number ___
+        // read and discard character to clear UART buffer
+        UARTCharGet(UART0_BASE);
 
+        // trigger ADC conversion
+        ADCProcessorTrigger(ADC0_BASE, 0);
+        while (!ADCIntStatus(ADC0_BASE, 0, false)) {} //wait for conversion to complete
+        ADCIntClear(ADC0_BASE, 0); //clear ADC interrupt flag
+        ADCSequenceDataGet(ADC0_BASE, 0, &INPUT); //get ADC result
 
+        // convert ADC value to voltage (0–3.3V)
+        voltage = (INPUT * 3.3f) / 4095.0f;
+        voltage_mV = (unsigned long)(voltage * 1000); //convert to millivolts
 
-while(1){ //All your code will go in this section
-    //read in ADC:
-    ADCProcessorTrigger(ADC0_BASE,0);        // trigger ADC convsersion
-    ADCSequenceDataGet(ADC0_BASE,0,&INPUT);  // get result
+	/* to print out the decimal, we must convert the number read from the pin to some kind of string
+	* in this case, we are subdividing the floating point number to its whole component (thousands) and 
+ 	* its decimal component (remainder). then, we will output each one using UART. 
+  	* Modulo is used to separate each 10ths place.     	
+  	*/
+	
+        int thousands = voltage_mV / 1000;
+        int remainder = voltage_mV % 1000;
 
+        UARTCharPut(UART0_BASE, '\n');
+        UARTCharPut(UART0_BASE, '\r');
 
-    //display voltage on the UART:
-  
-        //step 1 - convert the value to a set of characters
-        float voltage = (INPUT) / 4096;
-        mess_len = snprint(message, "%.2f", voltage);
-  
-        //Step 2 - output to UART
-        //reset position from last message
-        UARTCharPut(UART0_BASE,'\n');
-        UARTCharPut(UART0_BASE,'\r');
-  
-        //Voltage message
-        for (i=0; i<mess_len; i++){
+        //output voltage prefix message
+        for (i = 0; i < mess_len - 1; i++) {
             UARTCharPut(UART0_BASE, message[i]);
         }
 
-        // Voltage values
-  
-        // Voltage units
-        UARTCharPut(UART0_BASE, 'V');
+        //output integer component
+        UARTCharPut(UART0_BASE, '0' + thousands);
+        UARTCharPut(UART0_BASE, '.');
 
-        // Interface to trigger new sample read
-          // New line
-          UARTCharPut(UART0_BASE, '\n');
-  
-          // Output message about how to get a new sample
-          char sample_message[] = "Press button to get new sample";
-          for (i = 0; i < sizeof(sample_message) - 1; i++)
-          {
-              UARTCharPut(UART0_BASE, sample_message[i]);
-          }
-  
-          // Wait for button to be pushed to take next reading
-          while (!button_is_pressed())
-          {
-              // Wait loop (implement button_is_pressed() to return true when the button is pressed)
-          }
+        //output decimal component
+        UARTCharPut(UART0_BASE, '0' + ((remainder / 100) % 10));
+        UARTCharPut(UART0_BASE, '0' + ((remainder / 10) % 10));
+        UARTCharPut(UART0_BASE, '0' + (remainder % 10));
+	    
+        //output units
+        for (i = 0; i < un_len - 1; i++) {
+            UARTCharPut(UART0_BASE, units[i]);
+        }
+        // end of message
+	UARTCharPut(UART0_BASE, '\r');
+        UARTCharPut(UART0_BASE, '\n');
+        
+	// output repeat message
+        for (i = 0; i < next_len - 1; i++) {
+            UARTCharPut(UART0_BASE, next[i]);
+        }
+    }
+}
 
+// checks if a character is available from UART
+bool UARTKeyPressed(void) {
+    return UARTCharsAvail(UART0_BASE);
 }
