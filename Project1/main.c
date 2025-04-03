2c79cd82650baaba889db7eea9d5df70c179df9d
Shane Duffy <140999121+duffy-01@users.noreply.github.com>
2025-04-03 18:13:11 -0400

    Add files via upload

---
+++ Project1/main.c
@@ -0,0 +1,59 @@
+//*************************************************************************
+// Name of Group Members: Takeru Hiura, Shane Duffy
+// Creation Date: 4/3/25
+// Lab Section: Section 010
+// Lab this program is associated with: Project 1
+// Lab due date: 4/15/25
+//
+// Hardware Inputs used:
+// Hardware Outputs used:
+//
+// Additional files needed:
+//
+// Date of last modification: 4/3/25
+//*************************************************************************
+
+//****************************
+//  STATE STUFF
+//****************************
+
+// define state structures
+
+// state function prototypes
+    // next state
+
+// default NS light green
+
+//****************************
+//  IR SENSOR PARAMETERS
+//****************************
+
+// ACTIVE LOW!
+// IR Pins
+// IR Logic
+// IR Prototypes
+
+//****************************
+//  SWITCH PARAMETERS
+//****************************
+
+// Switch Pins
+// Switch Prototypes
+// Switch Logic
+
+
+
+
+/**
+ * main.c
+ */
+
+
+int main(void)
+{
+
+    while (1)
+	return 0;
+}
+
+// Function definitions
