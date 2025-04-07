fec8c552f544a8016df1d6d7eff19bfe8b8fa3a9
Shane Duffy <140999121+duffy-01@users.noreply.github.com>
2025-04-06 21:47:46 -0400

    Add files via upload

--- Project1/main.c
+++ Project1/main.c
@@ -10,12 +10,13 @@
 //
 // Additional files needed:
 //
-// Date of last modification: 4/3/25
+// Date of last modification: 4/6/25
 //*************************************************************************
 
 // include statements 
 #include <stdbool.h>
 #include <stdint.h>
+#include <string.h>
 #include "inc/tm4c123gh6pm.h"
 #include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
@@ -27,146 +28,162 @@
 #include "driverlib/uart.h"
 #include "driverlib/adc.h"
 
-//****************************
-//  STATE STUFF
-//****************************
+//*****************************************
+//          Constants and Globals
+//*****************************************
 
-const int CAR_MAX = 5;
+#define pinNS GPIO_PIN_0
+#define pinEW GPIO_PIN_1
+#define CAR_THRESHOLD 5
 
-typedef enum {STATE_NS, STATE_EW, OVERRIDE}
-trafficLightStates;
+#define NS_GREEN GPIO_PIN_2   // PF2
+#define NS_RED   GPIO_PIN_3   // PF3
+#define EW_GREEN GPIO_PIN_6   // PD6
+#define EW_RED   GPIO_PIN_7   // PD7
 
-// default NS light green
-void light_NS();
-void light_EW();
-void light_OVERRIDE();
+volatile int carCount = 0;
 
-//****************************
-//  IR SENSOR PARAMETERS
-//****************************
+enum Input {
+    SENSOR_NONE = 0,
+    SENSOR_NS   = 1,
+    SENSOR_EW   = 2,
+    SENSOR_BOTH = 3
+};
 
-const uint8_t pinNS; 
-const uint8_t pinEW;
+//*****************************************
+//          State Machine Stuff
+//*****************************************
 
-// ACTIVE LOW!
-void count_car(){}
-void count_car(){
+struct state {
+    unsigned long portF_out;  // North-South Lights (PF2, PF3)
+    unsigned long portD_out;  // East-West Lights (PD6, PD7)
+    const struct state *next[4];
+};
 
-}
+typedef const struct state styp;
 
+#define goN   &fsm[0]
+#define waitN &fsm[1]
+#define goE   &fsm[2]
+#define waitE &fsm[3]
+#define override &fsm[4]
 
-int carCount = 0; //global variable to count number of cars pass the sensor
+styp fsm[5] = {
+    {NS_GREEN,  EW_RED,   {goN, waitN, goN, waitN}},   // goN
+    {NS_RED,    EW_RED,   {goE, goE, goE, goE}},       // waitN
+    {NS_RED,    EW_GREEN, {goN, goE, waitE, waitE}},   // goE
+    {NS_RED,    EW_RED,   {goN, goN, goN, goN}},       // waitE
+    {NS_RED,    EW_RED,   {goN, goN, goN, goN}}        // override (all red)
+};
 
+styp *lightState = goN;
 
-// IR Pins
-// IR Logic
-// IR Prototypes
+//*****************************************
+//          Function Prototypes
+//*****************************************
 
-//****************************
-//  SWITCH PARAMETERS
-//****************************
+void portF_input_setup(uint8_t pins);
+void portD_input_setup(uint8_t pins);
+void systick(int reload_val);
+void pedestrian_override(void);
+void update_lights(unsigned long pf_out, unsigned long pd_out);
 
-// Switch Pins
-// Switch Prototypes
-// Switch Logic
+//*****************************************
+//          Main Function
+//*****************************************
 
-void portF_input_setup(uint8_t pins); // input setup function prototype
-void pedestrian_override(void); //pedestrian_override prototype
-void car_count(void);
+int main(void) {
+    uint8_t input;
+    carCount = 0;
+    lightState = goN;
 
-/*
- * main.c
- */
+    portF_input_setup(GPIO_PIN_4 | NS_GREEN | NS_RED);
+    portD_input_setup(pinNS | pinEW | EW_GREEN | EW_RED);
+    systick(0x00FFFFFF);
+    SysTickIntRegister(pedestrian_override);
+    IntMasterEnable();
 
-int main(void)
-{
-    portF_input_setup(0x10);            // call portF_input setup function to initialize PF4 (switch SW1) as input
-    portD_input_setup(pinNS | pinEW);   // setup the pins for NS and EW on port D
-   
-    systick(0x00FFFFFF);                //set reload value for SysTick
-    SysTickIntRegister(pedestrian_override);  //register SysTick interrupt handler
-    IntMasterEnable();                  //enable interrupts
-   
-    GPIOIntClear(GPIO_PORTF_BASE, GPIO_PIN_4);                      // clear the flag so the interrupt can happen again
-    GPIOIntTypeSet(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_FALLING_EDGE); // set interrupt type to falling edge for PF4 (switch SW1, active low)
-    GPIOIntRegister(GPIO_PORTF_BASE, pedestrian_override);          // register interrupt handler to pedestrian_override function
-    GPIOIntEnable(GPIO_PORTF_BASE, GPIO_PIN_4);                     // enable the interrupt
-    
-    while (1)
-    {
-        if (carCount >= CAR_MAX)
-        {
-            lightState = fsm[lightState.next[input]];
+    GPIOIntClear(GPIO_PORTF_BASE, GPIO_PIN_4);
+    GPIOIntTypeSet(GPIO_PORTF_BASE, GPIO_PIN_4, GPIO_FALLING_EDGE);
+    GPIOIntRegister(GPIO_PORTF_BASE, pedestrian_override);
+    GPIOIntEnable(GPIO_PORTF_BASE, GPIO_PIN_4);
+
+    while (1) {
+        input = 0;
+        if ((GPIOPinRead(GPIO_PORTD_BASE, pinNS) & pinNS) == 0) {
+            input |= 1;
+        }
+        if ((GPIOPinRead(GPIO_PORTD_BASE, pinEW) & pinEW) == 0) {
+            input |= 2;
+        }
+
+        // Count cars for the current direction
+        if ((input & 1) && (lightState == goN || lightState == waitN)) {
+            carCount++;
+            while ((GPIOPinRead(GPIO_PORTD_BASE, pinNS) & pinNS) == 0);
+        } else if ((input & 2) && (lightState == goE || lightState == waitE)) {
+            carCount++;
+            while ((GPIOPinRead(GPIO_PORTD_BASE, pinEW) & pinEW) == 0);
+        }
+
+        // Transition if enough cars have passed
+        if (carCount >= CAR_THRESHOLD) {
+            lightState = lightState->next[input];
+            update_lights(lightState->portF_out, lightState->portD_out);
             carCount = 0;
-        };
-
-        switch(lightState)
-        {
-            case STATE_NS:
-            // ir counter here
-            lightState = STATE_EW
-            case STATE_EW:
-
-            case OVERRIDE:
-            SysCtlDelay(SysCtlClockGet() / 3 * 10);
-            lightState = light_NS;
         }
     }
-    SysTickIntRegister();              //clear SysTick interrupt register
-	return 0;
+
+    return 0;
 }
 
-// Function definitions
-    // Start the function: portF_input_setup
-    // It is type void since it doesn't return anything
-    // It will have the following parameters: uint8_t pins, an 8 bit integer for the GPIO Port F pins we want to enable as input
-    // Step 1 of configuring a GPIO pin as an input is to configure the clock
-    // Step 2 of configuring a GPIO pin as an input is to unlock the switch
-    // Step 3 of configuration for a GPIO pin is to configure the direction to be an input
-    // Step 4 of the configuration for a GPIO pin to be input is to set up pull-up resistors for active low pins
-    // Step 5 of the configuration for a GPIO pin to be an output is to configure the enable for the pin
-    // end the function
-void portF_input_setup(uint8_t pins){
-    SYSCTL_RCGCGPIO_R |= 0x20; // configure the clock for Port F
-    GPIO_PORTF_LOCK_R |= 0x4C4F434B; // unlock pins
-    GPIO_PORTF_CR_R |= pins; // allow interaction with pins
-    GPIO_PORTF_DIR_R &= ~pins; // set direction for pins to be input
-    GPIO_PORTF_PUR_R |= pins; // set up pull-up resistors for active low pins
-    GPIO_PORTF_DEN_R |= pins; // configure the enable
+//*****************************************
+//         Functions Definitions
+//*****************************************
+
+void portF_input_setup(uint8_t pins) {
+    SYSCTL_RCGCGPIO_R |= 0x20;
+    GPIO_PORTF_LOCK_R = 0x4C4F434B;
+    GPIO_PORTF_CR_R |= pins;
+    GPIO_PORTF_DIR_R |= (pins & ~GPIO_PIN_4); // outputs except PF4
+    GPIO_PORTF_DIR_R &= ~GPIO_PIN_4; // PF4 is input
+    GPIO_PORTF_PUR_R |= GPIO_PIN_4;
+    GPIO_PORTF_DEN_R |= pins;
 }
 
-
-
-void portD_input_setup(uint8_t pins){
-    SYSCTL_RCGCGPIO_R |= 0x08; // configure the clock for Port D
-    GPIO_PORTD_CR_R |= pins; // allow interaction with pins
-    GPIO_PORTD_DIR_R &= ~pins; // set direction for pins to be input
-    GPIO_PORTD_PUR_R |= pins; // set up pull-up resistors for active low pins
-    GPIO_PORTD_DEN_R |= pins; // configure the enable
+void portD_input_setup(uint8_t pins) {
+    SYSCTL_RCGCGPIO_R |= 0x08;
+    GPIO_PORTD_CR_R |= pins;
+    GPIO_PORTD_DIR_R |= (pins & ~(pinNS | pinEW)); // outputs except IR inputs
+    GPIO_PORTD_DIR_R &= ~(pinNS | pinEW);
+    GPIO_PORTD_PUR_R |= (pinNS | pinEW);
+    GPIO_PORTD_DEN_R |= pins;
 }
 
-
-
-void systick(int reload_val){
-    NVIC_ST_CTRL_R = 0;                 //disable SysTick
-    NVIC_ST_RELOAD_R = reload_val;      //set reload value
-    NVIC_ST_CURRENT_R = 0;              //clear current value
-    NVIC_ST_CTRL_R = 0x07;              //enable SysTick with interrupts
+void systick(int reload_val) {
+    NVIC_ST_CTRL_R = 0;
+    NVIC_ST_RELOAD_R = reload_val;
+    NVIC_ST_CURRENT_R = 0;
+    NVIC_ST_CTRL_R = 0x07;
 }
 
-void pedestrian_override(void){
-
-    GPIOIntClear(GPIO_PORTF_BASE, GPIO_PIN_4); // clear the flag so the interrupt can happen again
-
-    if(!GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4)){ // check if switch SW1 is pressed
-        lightState = OVERRIDE;
-        SysCtlDelay(SysCtlClockGet() * 20 / 3); //create a delay of 20 seconds by multiplying the number of clocks per second by 10 and dividing by 3 because of the 3 cycle delay produced by SysCtlDelay
+void pedestrian_override(void) {
+    GPIOIntClear(GPIO_PORTF_BASE, GPIO_PIN_4);
+    if ((GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_4) & GPIO_PIN_4) == 0) {
+        lightState = override;
+        update_lights(lightState->portF_out, lightState->portD_out);
+        SysCtlDelay(SysCtlClockGet() * 5 / 3);
+        lightState = goN;
+        carCount = 0;
     }
 }
 
-void car_count(void){
-    if(GPIOPinREAD(GPIO_PORTD_BASE, GPIO_PIN_0) == 1){
-        carCount++;
-        while(GPIOPinREAD(GPIO_PORTD_BASE, GPIO_PIN_0) == 1);
-    }
+void update_lights(unsigned long pf_out, unsigned long pd_out) {
+    GPIO_PORTF_DATA_R &= ~(NS_GREEN | NS_RED);
+    GPIO_PORTF_DATA_R |= pf_out;
+
+    GPIO_PORTD_DATA_R &= ~(EW_GREEN | EW_RED);
+    GPIO_PORTD_DATA_R |= pd_out;
 }
+
+
